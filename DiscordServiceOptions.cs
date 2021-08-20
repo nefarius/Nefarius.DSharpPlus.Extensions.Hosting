fa11906b0f0253a82375295f71c2e02ce5b4f9d7
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-08-20 14:10:12 +0200

    Added example worker to play around with

---
+++ DiscordServiceOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nefarius.DSharpPlus.Extensions.Hosting
+{
+    public class DiscordServiceOptions
+    {
+    }
+}
