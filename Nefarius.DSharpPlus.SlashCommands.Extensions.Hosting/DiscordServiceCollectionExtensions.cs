52f92b2c1735ecda3af2f754aed7b252a4176660
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2021-08-24 22:34:35 +0200

    Added SlashCommands module

---
+++ Nefarius.DSharpPlus.SlashCommands.Extensions.Hosting/DiscordServiceCollectionExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using DSharpPlus.SlashCommands;
+using Emzi0767;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Nefarius.DSharpPlus.Extensions.Hosting;
+
+namespace Nefarius.DSharpPlus.SlashCommands.Extensions.Hosting
+{
+    [UsedImplicitly]
+    public static class DiscordServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Adds Interactivity extension to <see cref="IDiscordClientService" />.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection" />.</param>
+        /// <param name="configure">The <see cref="DiscordSlashCommandsOptions" />.</param>
+        /// <returns>The <see cref="IServiceCollection" />.</returns>
+        [UsedImplicitly]
+        public static IServiceCollection AddDiscordSlashCommands(
+            this IServiceCollection services,
+            Action<DiscordSlashCommandsOptions?> configure
+        )
+        {
+            services.AddSingleton(typeof(IDiscordExtensionConfiguration), provider =>
+            {
+                var options = new DiscordSlashCommandsOptions();
+
+                if (configure != null)
+                    configure(options);
+                else
+                    options.Configuration = new SlashCommandsConfiguration();
+
+                //
+                // Make all services available to bot commands
+                // 
+                options.Configuration.Services = provider;
+
+                var discord = provider.GetRequiredService<IDiscordClientService>().Client;
+
+                var ext = discord.UseSlashCommands(options.Configuration);
+
+                foreach (var (type, guildId) in options.CommandModules) ext.RegisterCommands(type, guildId);
+
+                //
+                // TODO: hook up events
+                // 
+
+                return options;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Nefarius.DSharpPlus.SlashCommands.Extensions.Hosting/DiscordSlashCommandsOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using DSharpPlus.SlashCommands;
+using JetBrains.Annotations;
+using Nefarius.DSharpPlus.Extensions.Hosting;
+
+namespace Nefarius.DSharpPlus.SlashCommands.Extensions.Hosting
+{
+    public class DiscordSlashCommandsOptions : IDiscordExtensionConfiguration
+    {
+        /// <summary>
+        ///     Sets the <see cref="SlashCommandsConfiguration" /> to use.
+        /// </summary>
+        public SlashCommandsConfiguration Configuration { internal get; set; }
+
+        /// <summary>
+        ///     Gets the list of <see cref="Type" />s to register as command modules.
+        /// </summary>
+        internal IDictionary<Type, ulong?> CommandModules { get; } = new Dictionary<Type, ulong?>();
+
+        /// <summary>
+        ///     Registers a command class.
+        /// </summary>
+        /// <typeparam name="T">The command class to register.</typeparam>
+        /// <param name="guildId">The guild id to register it on. If you want global commands, leave it null.</param>
+        [UsedImplicitly]
+        public void RegisterCommands<T>(ulong? guildId = null) where T : ApplicationCommandModule
+        {
+            CommandModules.Add(typeof(T), guildId);
+        }
+    }
+}
