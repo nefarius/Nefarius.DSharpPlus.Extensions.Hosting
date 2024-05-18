71fb80605a51c42ecce1b439310494394856e287
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2024-05-18 19:21:04 +0200

    Updated DSharpPlus to 5.0.0-nightly-02261
    Fixed API breaking change

--- WorkerExample/AdminCommands.cs
+++ WorkerExample/AdminCommands.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
@@ -9,7 +8,7 @@ namespace WorkerExample
     [Group("admin")] // let's mark this class as a command group
     [Description("Administrative commands.")] // give it a description for help purposes
     [Hidden] // let's hide this from the eyes of curious users
-    [RequirePermissions(Permissions.ManageGuild)] // and restrict this to users who have appropriate permissions
+    [RequirePermissions(DiscordPermissions.ManageGuild)] // and restrict this to users who have appropriate permissions
     public class AdminCommands : BaseCommandModule
     {
         // all the commands will need to be executed as <prefix>admin <command> <arguments>
